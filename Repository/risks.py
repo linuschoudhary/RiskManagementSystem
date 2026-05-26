7412d70664d17d25019682c1e4f270a317db1df0
linuschoudhary <scjat1133@gmail.com>
2026-05-26 15:18:38 +0530

    Added Not Assigned insted of None to users in risks

--- Repository/risks.py
+++ Repository/risks.py
@@ -19,22 +19,22 @@ def get_all(db:Session):
                 "risk_type" : risk.risk_type,
                 "created_by" : {
                     "user_id" : risk.created_by,
-                    "user_name" : risk.creator.user_name if risk.creator else None,
-                    "user_role" : risk.creator.user_role if risk.creator else None,
-                    "user_email": risk.creator.user_email if risk.creator else None
-                } if risk.created_by else None,
+                    "user_name" : risk.creator.user_name if risk.creator else "Not Assigned",
+                    "user_role" : risk.creator.user_role if risk.creator else "Not Assigned",
+                    "user_email": risk.creator.user_email if risk.creator else "Not Assigned"
+                } if risk.created_by else "Not Assigned",
                 "risk_allocation" : {
                     "user_id" : risk.risk_allocation,
-                    "user_name" : risk.allocator.user_name if risk.allocator else None,
-                    "user_role" : risk.allocator.user_role if risk.allocator else None,
-                    "user_email": risk.allocator.user_email if risk.allocator else None
-                } if risk.risk_allocation else None,
+                    "user_name" : risk.allocator.user_name if risk.allocator else "Not Assigned",
+                    "user_role" : risk.allocator.user_role if risk.allocator else "Not Assigned",
+                    "user_email": risk.allocator.user_email if risk.allocator else "Not Assigned"
+                } if risk.risk_allocation else "Not Assigned",
                 "assigned_to" : {
                     "user_id" : risk.assigned_to,
-                    "user_name" : risk.assignee.user_name if risk.assignee else None,
-                    "user_role" : risk.assignee.user_role if risk.assignee else None,
-                    "user_email": risk.assignee.user_email if risk.assignee else None
-                } if risk.assigned_to else None,
+                    "user_name" : risk.assignee.user_name if risk.assignee else "Not Assigned",
+                    "user_role" : risk.assignee.user_role if risk.assignee else "Not Assigned",
+                    "user_email": risk.assignee.user_email if risk.assignee else "Not Assigned"
+                } if risk.assigned_to else "Not Assigned",
                 "risk_category": risk.risk_category,
                 "due_date": risk.due_date
                 
@@ -58,22 +58,22 @@ def get_risks_by_id(db:Session,risk_id:int):
         "risk_type" : risk.risk_type,
         "created_by" : {
             "user_id" : risk.created_by,
-            "user_name" : risk.creator.user_name if risk.creator else None,
-            "user_role" : risk.creator.user_role if risk.creator else None,
-            "user_email": risk.creator.user_email if risk.creator else None
-        } if risk.created_by else None,
+            "user_name" : risk.creator.user_name if risk.creator else "Not Assigned",
+            "user_role" : risk.creator.user_role if risk.creator else "Not Assigned",
+            "user_email": risk.creator.user_email if risk.creator else "Not Assigned"
+        } if risk.created_by else "Not Assigned",
         "risk_allocation" : {
             "user_id" : risk.risk_allocation,
-            "user_name" : risk.allocator.user_name if risk.allocator else None,
-            "user_role" : risk.allocator.user_role if risk.allocator else None,
-            "user_email": risk.allocator.user_email if risk.allocator else None
-        } if risk.risk_allocation else None,
+            "user_name" : risk.allocator.user_name if risk.allocator else "Not Assigned",
+            "user_role" : risk.allocator.user_role if risk.allocator else "Not Assigned",
+            "user_email": risk.allocator.user_email if risk.allocator else "Not Assigned"
+        } if risk.risk_allocation else "Not Assigned",
         "assigned_to" : {
             "user_id" : risk.assigned_to,
-            "user_name" : risk.assignee.user_name if risk.assignee else None,
-            "user_role" : risk.assignee.user_role if risk.assignee else None,
-            "user_email": risk.assignee.user_email if risk.assignee else None
-        } if risk.assigned_to else None,
+            "user_name" : risk.assignee.user_name if risk.assignee else "Not Assigned",
+            "user_role" : risk.assignee.user_role if risk.assignee else "Not Assigned",
+            "user_email": risk.assignee.user_email if risk.assignee else "Not Assigned"
+        } if risk.assigned_to else "Not Assigned",
         "risk_category": risk.risk_category,
         "due_date": risk.due_date
     }
